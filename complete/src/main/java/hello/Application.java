2fb0c2170609a2b5af9ceb173f83595a88aac6db
keeslouwen <kees.louwen@vancis.nl>
2019-05-02 12:16:25 +0200

    servlet wildfly imports

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -3,8 +3,11 @@ package hello;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Spring Boot 2.x
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
